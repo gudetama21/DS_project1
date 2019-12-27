5a44fa4bf86638e710bc2a38e298765b7751e8fa
gudetama21 <vbook20@gmail.com>
2019-12-27 21:10:22 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		WebPage rootPage = new WebPage("http://www3.nccu.edu.tw/~yuf", "Fang Yu");		
 		WebTree tree = new WebTree(rootPage);
 		
 		tree.root.addChild(new WebNode(new WebPage("http://soslab.nccu.edu.tw/Publications.html","Publication")));
 		tree.root.addChild(new WebNode(new WebPage("http://soslab.nccu.edu.tw/Projects.html","Project")));
 		tree.root.children.get(1).addChild(new WebNode(new WebPage("http://www.cs.ucsb.edu/~vlab/stranger/", "Stranger")));
-//		tree.root.children.get(1).addChild(new WebNode(new WebPage("http://nccusoslab.com:280/data-structure/little-boat.html", "LittleBoat")));
 		tree.root.addChild(new WebNode(new WebPage("http://www3.nccu.edu.tw/~yuf/short_biography.htm","Biography")));
 		tree.root.children.get(2).addChild(new WebNode(new WebPage("http://www.cs.ucsb.edu/~vlab", "Vlab")));
 		tree.root.addChild(new WebNode(new WebPage("http://www3.nccu.edu.tw/~yuf/course.htm","Course")));
@@ -38,22 +36,3 @@ public class Main {
 
 }
 
-
-/*
- * 
-Input:
-2
-Fang 0.5
-Yu 0.6
-Output:
-(Fang Yu,131.10000000000002
-	(Publication,110.5)
-	(Project,12.1
-		(Stranger,0.0)
-	)
-	(Biography,2.8
-		(Vlab,0.0)
-	)
-	(Course,2.3)
-)
-*/
